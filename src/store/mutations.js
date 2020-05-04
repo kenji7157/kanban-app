31903e1bb6ce7d6f76752d29817a0adbb1f51a19
Kenji Kawanobe <kenji_kawanobe@jsl.co.jp>
2020-05-04 17:26:05 +0900

    データフロー周りの雛形コードのセットアップ

---
+++ src/store/mutations.js
@@ -0,0 +1,28 @@
+import * as types from './mutation-types'
+
+export default {
+  [types.AUTH_LOGIN] (state, payload) {
+    // TODO
+    throw new Error('AUTH_LOGIN mutation shooould be implementd')
+  },
+  [types.FETCH_ALL_TASKLIST] (state, payload) {
+    // TODO
+    throw new Error('FETCH_ALL_TASKLIST mutation shooould be implementd')
+  },
+  [types.ADD_TASK] (state, payload) {
+    // TODO
+    throw new Error('ADD_TASK mutation shooould be implementd')
+  },
+  [types.UPDATE_TASK] (state, payload) {
+    // TODO
+    throw new Error('UPDATE_TASK mutation shooould be implementd')
+  },
+  [types.REMOVE_TASK] (state, payload) {
+    // TODO
+    throw new Error('REMOVE_TASK mutation shooould be implementd')
+  },
+  [types.AUTH_LOGOUT] (state) {
+    // TODO
+    throw new Error('AUTH_LOGOUT mutation shooould be implementd')
+  }
+}
