2e145650aefa3de9097b55c4a3932291d72a3413
Kenji Kawanobe <kenji_kawanobe@jsl.co.jp>
2020-05-10 23:42:16 +0900

    開発サーバとデバック

--- build/dev-server.js
+++ build/dev-server.js
@@ -1,10 +1,32 @@
 // Node.jsのrequireスタイルでインポート
-const boodyParser = require('body-parser')
+const bodyParser = require('body-parser')
 
-// Expressアプリケーションインスタンスを受け取る関数をエクスポート
+// `Express`アプリケーションインスタンスを受取る関数をエクスポート
 module.exports = app => {
-  // HTTPリクエストのbodyの内容をJSONとして解析するミドルウェアをエクスポート
-  app.use(boodyParser.json())
+  // HTTPリクエストのbodyの内容をJSONとして解析するようミドルウェアをインストール
+  app.use(bodyParser.json())
 
-  // ここ以降にAPIの実装内容を記載していく
+  // ユーザー情報
+  const users = {
+    'foo@domain.com': {
+      password: '12345678',
+      userId: 1,
+      token: '1234567890abcdef'
+    }
+  }
+
+  // ログインAPIのエンドポイント'/auth/login'
+  app.post('/auth/login', (req, res) => {
+    const { email, password } = req.body
+    const user = users[email]
+    if (user) {
+      if (user.password !== password) {
+        res.status(401).json({ message: 'ログインに失敗しました。' })
+      } else {
+        res.json({ userId: user.userId, token: user.token })
+      }
+    } else {
+      res.status(404).json({ message: 'ユーザーが登録されていません。' })
+    }
+  })
 }
